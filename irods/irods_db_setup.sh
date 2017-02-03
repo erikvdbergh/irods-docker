5b11f5d15362ac918d87f7eb3fa19343dfb45ff2
erikvdbergh <erikvdbergh@gmail.com>
2017-02-03 16:00:12 +0000

    fix hostnames, fix compose

--- irods/irods_db_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hostname=`hostname`
-
-psql -h postgres -U irods ICAT -c "UPDATE r_resc_main SET resc_net='$hostname' WHERE resc_id=9101"
-
-/var/lib/irods/packaging/./setup_irods.sh < setup_answers
-
-tail -f /var/lib/irods/iRODS/server/log/rodsLog.*
