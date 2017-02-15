c0937e03f0602fa51e4413936813591333943f48
erikvdbergh <erikvdbergh@gmail.com>
2017-02-15 16:36:09 +0000

    get pass from file

--- postgres/setup_irods_db.sh
+++ postgres/setup_irods_db.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+password=`cat password`
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER irods WITH PASSWORD 'testpassword';
+    CREATE USER irods WITH PASSWORD '$password';
     CREATE DATABASE "ICAT";
     GRANT ALL PRIVILEGES ON DATABASE "ICAT" TO irods;
 EOSQL
